66310989c51a10818ff98619c763a2bce5f4cb76
1t1nu5 <78350770+1t1nu5@users.noreply.github.com>
2023-02-11 19:37:53 +0700

    มีหน้าที่เป็นเหมือนบทละคร สั่งให้โปรแกรมทำตาม
    เป็นส่วนที่ขาดไปไม่ได้ และก็เป็นส่วนที่ต้องมีไฟล์แพ็กเกจ control ก่อน จึงจะทำงานได้

---
+++ Script.java
@@ -0,0 +1,26 @@
+package stage;
+
+import java.awt.Graphics;
+
+public class Script {
+	
+	public enum MGroup {
+		MAIN_BOTTOM, MAIN_TOP
+	}
+
+	public static void load() {
+		;
+	}
+
+	public static void setup() {
+		;
+	}
+
+	public static void loopBottom(Graphics g) {
+		;
+	}
+
+	public static void loopTop(Graphics g) {
+		;
+	}
+}
