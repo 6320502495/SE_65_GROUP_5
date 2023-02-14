8e5c86b5e6913a6f8a4fbde4015f4730e01a7ae6
ณัชพล จำปานิน <78350770+1t1nu5@users.noreply.github.com>
2023-02-15 01:37:33 +0700

    เพิ่มเมธอด FileToHexString
    ซึ่งมีหน้าที่แปลงไฟล์ให้กลายเป็นชุดเลขฐานสิบหกในรูปของ String เพื่อนำไปใช้ Query

--- control/Server.java
+++ control/Server.java
@@ -1,6 +1,7 @@
 package control;
 
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
@@ -10,6 +11,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+
+import javax.sql.rowset.serial.SerialException;
 
 public class Server {
 	private static Connection connection = null;
@@ -52,29 +55,97 @@ public class Server {
 		TINY, NORMAL, MEDIUM, LONG
 	}
 
+	public static final String FileToHexString(File file) throws IOException, SerialException, SQLException {
+		byte[] bytes = new byte[(int) file.length()];
+		FileInputStream fileInputs = null;
+		try {
+			fileInputs = new FileInputStream(file);
+			fileInputs.read(bytes);
+		} finally {
+			if (fileInputs != null)
+				fileInputs.close();
+		}
+		short[] bin = new short[bytes.length];
+		for (int i = 0; i < bytes.length; i++)
+			bin[i] = (short) (bytes[i] & 0xFF);
+		byte[] hex = new byte[bin.length * 2];
+		for (int i = 0; i < bin.length; i++) {
+			short number[] = new short[2];
+			number[1] = (short) (bin[i] % 16);
+			number[0] = (short) Math.floorDiv(bin[i], 16);
+			switch (number[0]) {
+			case 10:
+				hex[2*i] = 'a';
+				break;
+			case 11:
+				hex[2*i] = 'b';
+				break;
+			case 12:
+				hex[2*i] = 'c';
+				break;
+			case 13:
+				hex[2*i] = 'd';
+				break;
+			case 14:
+				hex[2*i] = 'e';
+				break;
+			case 15:
+				hex[2*i] = 'f';
+				break;
+			default:
+				hex[2*i] = (byte) (number[0] + '0');
+				break;
+			}
+			switch (number[1]) {
+			case 10:
+				hex[2*i+1] = 'a';
+				break;
+			case 11:
+				hex[2*i+1] = 'b';
+				break;
+			case 12:
+				hex[2*i+1] = 'c';
+				break;
+			case 13:
+				hex[2*i+1] = 'd';
+				break;
+			case 14:
+				hex[2*i+1] = 'e';
+				break;
+			case 15:
+				hex[2*i+1] = 'f';
+				break;
+			default:
+				hex[2*i+1] = (byte) (number[1] + '0');
+				break;
+			}
+		}
+		return "0x" + new String(hex);
+	}
+
 	public static final File blobToFile(Blob blob, BlobType blobType, String saveName)
 			throws IOException, SQLException {
-		InputStream in = blob.getBinaryStream();
-		OutputStream out = new FileOutputStream(MPainter.imagePath() + saveName);
-		byte[] buff = null;
+		InputStream inputs = blob.getBinaryStream();
+		OutputStream outputs = new FileOutputStream(MPainter.imagePath() + saveName);
+		byte[] buffer = null;
 		switch (blobType) {
 		case TINY:
-			buff = new byte[255];
+			buffer = new byte[255];
 			break;
 		case NORMAL:
-			buff = new byte[65535];
+			buffer = new byte[65535];
 			break;
 		case MEDIUM:
-			buff = new byte[16777215];
+			buffer = new byte[16777215];
 			break;
 		case LONG:
-			buff = new byte[1048576 * 4194304 - 1];
+			buffer = new byte[1048576 * 4194304 - 1];
 			break;
 		}
-		int len = 0;
-		while ((len = in.read(buff)) != -1)
-			out.write(buff, 0, len);
-		out.close();
+		int length = 0;
+		while ((length = inputs.read(buffer)) != -1)
+			outputs.write(buffer, 0, length);
+		outputs.close();
 		return new File(MPainter.imagePath() + saveName);
 	}
 }
