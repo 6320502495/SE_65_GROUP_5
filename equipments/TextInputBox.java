2410d35262038336cf7a0c3ed94820cbc72af0a3
ณัชพล จำปานิน <78350770+1t1nu5@users.noreply.github.com>
2023-02-15 06:08:25 +0700

    เพิ่ม BufferedImage ให้วาดภาพได้เร็วขึ้น

--- equipments/TextInputBox.java
+++ equipments/TextInputBox.java
@@ -5,6 +5,7 @@ import java.awt.Font;
 import java.awt.Graphics;
 import java.awt.Point;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
@@ -41,14 +42,17 @@ public class TextInputBox extends MObject {
 			throw new IllegalArgumentException("font cannot be null");
 		mButton = new MButton(mGroup, point, figure, images, false, false) {
 			private String[] images;
+			private BufferedImage[] bufferedImages = new BufferedImage[8];
 
-			public void setImages(String[] images) {
+			public void setImages(String[] images) throws IOException {
 				if (images.length != 8)
 					throw new IllegalArgumentException("images's length must be 8");
 				for (int i = 0; i < 8; i++)
 					if (images[i] == null)
 						throw new IllegalArgumentException("images cannot be null");
 				this.images = images;
+				for (int i = 0; i < 8; i++)
+					bufferedImages[i] = ImageIO.read(new File(MPainter.imagePath() + images[i]));
 			}
 
 			@Override
@@ -66,29 +70,21 @@ public class TextInputBox extends MObject {
 				try {
 					if (mFrame.isTouched())
 						if (triggered)
-							g.drawImage(ImageIO.read(new File(MPainter.imagePath() + images[3])), mFrame.point().x,
-									mFrame.point().y, null);
+							g.drawImage(bufferedImages[3], mFrame.point().x, mFrame.point().y, null);
 						else if (textInputter.read().compareTo("") == 0)
-							g.drawImage(ImageIO.read(new File(MPainter.imagePath() + images[1])), mFrame.point().x,
-									mFrame.point().y, null);
+							g.drawImage(bufferedImages[1], mFrame.point().x, mFrame.point().y, null);
 						else if (legal)
-							g.drawImage(ImageIO.read(new File(MPainter.imagePath() + images[5])), mFrame.point().x,
-									mFrame.point().y, null);
+							g.drawImage(bufferedImages[5], mFrame.point().x, mFrame.point().y, null);
 						else
-							g.drawImage(ImageIO.read(new File(MPainter.imagePath() + images[7])), mFrame.point().x,
-									mFrame.point().y, null);
+							g.drawImage(bufferedImages[7], mFrame.point().x, mFrame.point().y, null);
 					else if (triggered)
-						g.drawImage(ImageIO.read(new File(MPainter.imagePath() + images[2])), mFrame.point().x,
-								mFrame.point().y, null);
+						g.drawImage(bufferedImages[2], mFrame.point().x, mFrame.point().y, null);
 					else if (textInputter.read().compareTo("") == 0)
-						g.drawImage(ImageIO.read(new File(MPainter.imagePath() + images[0])), mFrame.point().x,
-								mFrame.point().y, null);
+						g.drawImage(bufferedImages[0], mFrame.point().x, mFrame.point().y, null);
 					else if (legal)
-						g.drawImage(ImageIO.read(new File(MPainter.imagePath() + images[4])), mFrame.point().x,
-								mFrame.point().y, null);
+						g.drawImage(bufferedImages[4], mFrame.point().x, mFrame.point().y, null);
 					else
-						g.drawImage(ImageIO.read(new File(MPainter.imagePath() + images[6])), mFrame.point().x,
-								mFrame.point().y, null);
+						g.drawImage(bufferedImages[6], mFrame.point().x, mFrame.point().y, null);
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
