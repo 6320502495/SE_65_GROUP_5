b463a251c3588e362dd4b85dfaf0b979b02a48c6
ณัชพล จำปานิน <78350770+1t1nu5@users.noreply.github.com>
2023-02-16 02:40:39 +0700

    optimize การวาดภาพ และเพิ่มพื้นหลังสีดำ

--- equipments/FixedBackground.java
+++ equipments/FixedBackground.java
@@ -1,25 +1,34 @@
 package equipments;
 
+import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.image.BufferedImage;
-import java.io.File;
 import java.io.IOException;
 
-import javax.imageio.ImageIO;
-
+import control.FileManager;
 import control.MPainter;
 
 public class FixedBackground extends Background {
 	private BufferedImage bufferedImage;
+	private int x = 0;
+	private int y = 0;
 	
 	public FixedBackground(String image) throws IOException {
 		if (image == null)
 			throw new IllegalArgumentException("image cannot be null");
-		bufferedImage = ImageIO.read(new File(MPainter.imagePath() + image));
+		bufferedImage = FileManager.readImage(image);
 	}
 
 	@Override
 	public void loop(Graphics g) {
-		g.drawImage(bufferedImage, 0, 0, null);
+		g.setColor(Color.BLACK);
+		g.fillRect(0, 0, MPainter.WIDTH, MPainter.HEIGHT);
+		x++;
+		if (x >= 100)
+			x = 0;
+		y++;
+		if (y >= 100)
+			y = 0;
+		g.drawImage(bufferedImage, x, y, null);
 	}
 }
