0eb885a15175778a8efd86a2fb6d5687786738fb
b6320503033 <78350248+b6320503033@users.noreply.github.com>
2023-03-12 10:47:41 +0700

    Add files via upload

---
+++ divvy/app/Http/Controllers/AnonymousController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class AnonymousController extends Controller
+{
+    function home(){
+        return view('anonymous.home');
+    }
+}
