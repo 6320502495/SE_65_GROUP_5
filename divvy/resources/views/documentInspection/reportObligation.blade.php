36875efb160537109ae6a20ea9a30839bf7f36e3
ณัชพล จำปานิน <78350770+1t1nu5@users.noreply.github.com>
2023-03-27 02:26:32 +0700

    หน้าทำตามคำร้องเรียน ชั่วคราว

---
+++ divvy/resources/views/documentInspection/reportObligation.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <title>หน้าเอกสาร (ทำตามคำร้อง)</title>
+    </head>
+    <body>
+        <h1>สำหรับผู้ดูแลระบบ</h1>
+    </body> 
+</html>
