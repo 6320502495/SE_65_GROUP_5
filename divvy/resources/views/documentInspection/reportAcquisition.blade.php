53db11a5a8c69a44b1e9f4bf3acb6312c2a7e777
ณัชพล จำปานิน <78350770+1t1nu5@users.noreply.github.com>
2023-03-27 02:25:54 +0700

    หน้ารับการร้องเรียน ชั่วคราว
    สำหรับผู้ตรวจสอบเอกสาร

---
+++ divvy/resources/views/documentInspection/reportAcquisition.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <title>หน้าเอกสาร (รับคำร้อง)</title>
+    </head>
+    <body>
+        <h1>สำหรับผู้ตรวจสอบการร้องเรียน</h1>
+    </body> 
+</html>
