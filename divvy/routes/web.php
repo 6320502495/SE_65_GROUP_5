d7fc1209f13115f43ba8148997947db928f97a03
ณัชพล จำปานิน <78350770+1t1nu5@users.noreply.github.com>
2023-03-27 02:18:05 +0700

    สร้าง Route สำหรับผู้ตรวจสอบเอกสารและผู้ดูแลระบบ
    โดยจะต้องมีการกรอกรหัสผ่านไปที่ url เพื่อความปลอดภัย

--- divvy/routes/web.php
+++ divvy/routes/web.php
@@ -42,3 +42,20 @@ Route::get('/campaign/edit/{id}', [CampaignController::class, 'edit'])->name('ca
 Route::post('/campaign/update/{id}', [CampaignController::class, 'update'])->name('campaignUpdate');
 Route::get('/campaign/all', [CampaignController::class, 'showAll'])->name('campaignAll');
 Route::get('/campaign/{id}', [CampaignController::class, 'show'])->name('campaign');
+
+/* Document Inspection */
+Route::get('/documentInspection/{password}', function ($password) {
+    if (password_verify($password, '$2y$10$MHIsDHSbSAjnJQ.CbnBTRurAkGC6dGpFIZ/uInce7.OvDN18CBt5S')) {
+        // password: documentinspectorpassword
+        return view('documentInspection/reportAcquisition');
+    } else if (password_verify($password, '$2y$10$hKkp36BE38F6hMb0C9nMp.0SKNo1QHm29TPo8BNg1tk7GbJIo6w7q')) {
+        // password: adminpassword
+        return view('documentInspection/reportObligation');
+    }
+    // เมื่อ Divvy ทำเสร็จสมบูรณ์ จะต้องลบรหัสผ่านที่ไม่ได้ถูก hash ออกไปจากโค้ดเพื่อความปลอดภัย
+});
+
+/*  $hashed_password = password_hash($password, PASSWORD_DEFAULT);
+    โค้ดสำหรับการ hash $password ให้กลายเป็น $hashed_password
+    password_verify($user_password, $stored_hashed_password)
+    โค้ดตรวจสอบว่า $user_password ตรงกับ $stored_hashed_password หรือไม่ */
