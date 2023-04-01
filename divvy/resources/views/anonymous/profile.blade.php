2e0deb4331632626dc456d09ca0d1fef5011ac7f
b6320503033 <78350248+b6320503033@users.noreply.github.com>
2023-04-01 20:01:31 +0700

    Add files via upload

---
+++ divvy/resources/views/anonymous/profile.blade.php
@@ -0,0 +1,119 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" type="text/css">
+  <link rel="stylesheet" href="https://static.pingendo.com/bootstrap/bootstrap-4.3.1.css">
+</head>
+
+<body>
+  <nav class="navbar navbar-expand-md navbar-dark bg-primary">
+    <div class="container"> <button class="navbar-toggler navbar-toggler-right border-0" type="button" data-toggle="collapse" data-target="#navbar18">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbar18"> <a class="navbar-brand d-none d-md-block" href="#">
+          <i class="fa d-inline fa-lg fa-circle"></i>
+          <b> Divvy</b>
+        </a>
+        <ul class="navbar-nav mx-auto"></ul>
+        <ul class="navbar-nav">
+          <li class="nav-item"> <a class="nav-link" href="#">Profile</a> </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="py-5">
+    <div class="container w-100 text-center">
+      <div class="row">
+        <div class="col-3">
+          <ul class="nav nav-pills flex-column">
+            <li class="nav-item" > <a href="" class="active nav-link" data-toggle="pill" data-target="#tabone">Profile</a> </li>
+            <li class="nav-item"> <a class="nav-link" href="" data-toggle="pill" data-target="#tabtwo">Verify</a> </li>
+            <li class="nav-item"> <a href="" class="nav-link" data-toggle="pill" data-target="#tabthree">History</a> </li>
+          </ul>
+        </div>
+        <div class="col-9">
+          <div class="tab-content">
+            <div class="tab-pane fade show active" id="tabone" role="tabpanel">
+              <img class="img-fluid d-block rounded-circle mx-auto" src="https://static.pingendo.com/img-placeholder-3.svg" width="100
+200">
+              <div class="row mt-4">
+                <div class="col-md-6">
+                  <p class="lead text-right">Name :</p>
+                </div>
+                <div class="col-md-6">
+                  <p class="lead text-left">User123</p>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-6">
+                  <p class="lead text-right">Account Name :</p>
+                </div>
+                <div class="col-md-6">
+                  <p class="lead text-left">uer123xbctj89</p>
+                </div>
+              </div>
+              <div class="row">
+                <div class="col-md-6">
+                  <p class="lead text-right">Amount :</p>
+                </div>
+                <div class="col-md-6">
+                  <p class="lead text-left">100.00</p>
+                </div>
+              </div><a class="btn btn-primary mt-4" href="#">เติมเงิน</a>
+            </div>
+            <div class="tab-pane fade" id="tabtwo" role="tabpanel">
+              <p class="">test123456</p>
+            </div>
+            <div class="tab-pane fade" id="tabthree" role="tabpanel" style="">
+              <div class="table-responsive">
+                <table class="table">
+                  <thead>
+                    <tr>
+                      <th>ID No.</th>
+                      <th>Date</th>
+                      <th>Time</th>
+                      <th>Campaign Name</th>
+                      <th>Amount</th>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1</td>
+                      <td>2/2/2023</td>
+                      <td>13.00</td>
+                      <td>Otto</td>
+                      <td>100.00</td>
+                    </tr>
+                    <tr>
+                      <td>2</td>
+                      <td>4/2/2023</td>
+                      <td>16.00</td>
+                      <td>Thornton</td>
+                      <td>200.00</td>
+                    </tr>
+                    <tr>
+                      <td>3</td>
+                      <td>3/3/2023</td>
+                      <td>17.00</td>
+                      <td>the Bird</td>
+                      <td contenteditable="true">300.00</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+  <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
+  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+  <pingendo onclick="window.open('https://pingendo.com/', '_blank')" style="cursor:pointer;position: fixed;bottom: 20px;right:20px;padding:4px;background-color: #00b0eb;border-radius: 8px; width:220px;display:flex;flex-direction:row;align-items:center;justify-content:center;font-size:14px;color:white">Made with Pingendo Free&nbsp;&nbsp;<img src="https://pingendo.com/site-assets/Pingendo_logo_big.png" class="d-block" alt="Pingendo logo" height="16"></pingendo>
+</body>
+
+</html>
