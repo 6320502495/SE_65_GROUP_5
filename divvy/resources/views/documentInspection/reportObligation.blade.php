2198c34acdeeb4696b76cf7a97f4612a866a5b73
ณัชพล จำปานิน <78350770+1t1nu5@users.noreply.github.com>
2023-04-02 17:10:45 +0700

    เพิ่มให้ Admin เห็นบัญชีที่ถูกส่งต่อได้

--- divvy/resources/views/documentInspection/reportObligation.blade.php
+++ divvy/resources/views/documentInspection/reportObligation.blade.php
@@ -4,6 +4,28 @@
         <title>หน้าเอกสาร (ทำตามคำร้อง)</title>
     </head>
     <body>
-        <h1>สำหรับผู้ดูแลระบบ</h1>
+        <h1>หน้าสำหรับผู้ดูแลระบบ</h1>
+        <?php
+            use App\Http\Controllers\DocumentInspectionController;
+            $reportUsers = DocumentInspectionController::showAllForAdmin();
+            echo "<table border=1>";
+                echo "<tr><th>ชื่อบัญชี</th><th>ชื่อ</th><th>นามสกุล</th><th>วันเกิด</th><th>สาเหตุที่ถูกร้องเรียน</th><th colspan=2>ตัวเลือก</th></tr>";
+                foreach($reportUsers as $reportUser) {
+                    echo "<tr>";
+                    echo "<td>", $reportUser->Account_Name, "</td>";
+                        echo "<td>", $reportUser->Account_Firstname, "</td>";
+                        echo "<td>", $reportUser->Account_Surname, "</td>";
+                        echo "<td>", $reportUser->Account_Birthday, "</td>";
+                        echo "<td>", $reportUser->Report_Reason, "</td>";
+                        echo "<td>";
+                            echo "ปฏิเสธการร้องเรียน";
+                        echo "</td>";
+                        echo "<td>";
+                            echo "ส่งต่อการร้องเรียน";
+                        echo "</td>";
+                    echo "</tr>";
+                };
+            echo "</table>";
+        ?>
     </body> 
 </html>
