94698ffd9cac2ed0fd1fbe76f22c428e3bb0ed44
ณัชพล จำปานิน <78350770+1t1nu5@users.noreply.github.com>
2023-04-03 16:03:26 +0700

    ลืมอ้าง path ของ controller

--- divvy/routes/web.php
+++ divvy/routes/web.php
@@ -8,6 +8,7 @@ use App\Http\Controllers\AccountController;
 use App\Http\Controllers\CampaignController;
 use App\Http\Controllers\AdminController;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\DocumentInspectionController;
 
 /*
 |--------------------------------------------------------------------------
