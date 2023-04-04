da3d79c62172a764f82c7a4024f404eaf95a1124
b6320503033 <78350248+b6320503033@users.noreply.github.com>
2023-04-04 22:57:42 +0700

    Update AnonymousController.php

--- divvy/app/Http/Controllers/AnonymousController.php
+++ divvy/app/Http/Controllers/AnonymousController.php
@@ -1,13 +1,40 @@
-/*ไม่ได้ใช้*/
 <?php
 
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+use Illuminate\View\View;
+use App\Models\Campaign;
+use App\Models\DonationRecord;
+use Illuminate\Support\Facades\Auth;
+
 
 class AnonymousController extends Controller
 {
     function home(){
-        return view('anonymous.home');
+        $campaign=DB::table('campaign')->get();
+
+       $donation_rec=DB::table('donation_record')->get();
+        
+        return view('welcome', compact('campaign','donation_rec'));
     }
+
+    public function show(int $Campaign_ID) : View
+    {
+        $campaign=Campaign::find($Campaign_ID);
+        
+        $total_donation=$total_donations = DB::table('donation_record')
+        ->where('Campaign_ID', $Campaign_ID)
+        ->sum('Amount');
+        
+        return view('anonymousCampaign', compact('campaign','total_donation'));
+    }
+   
+    
+    
+    
 }
+
+
+?>
